bc6099c5adcc270ec601ee18abc5e46324e24eeb
nicolasalexferrari <nicolasalexferrari@gmail.com>
2026-08-22 19:45:15 +0000

    mensagem explicando o que mudou

--- main.tf
+++ main.tf
@@ -10,3 +10,32 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+resource "aws_s3_bucket" "secure_bucket" {
+  bucket = "empresa-dados-seguros-exemplo"
+}
+
+resource "aws_s3_bucket_public_access_block" "secure_bucket_block" {
+  bucket = aws_s3_bucket.secure_bucket.id
+
+  block_public_acls       = true
+  ignore_public_acls      = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+}
+resource "aws_s3_bucket_server_side_encryption_configuration" "secure_bucket_encryption" {
+  bucket = aws_s3_bucket.secure_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_versioning" "secure_bucket_versioning" {
+  bucket = aws_s3_bucket.secure_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
